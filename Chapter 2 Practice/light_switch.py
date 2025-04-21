73fa7083f7caea67548054b673c78a4bf78a37b2
MalikUmair5 <malikumair.dev@gmail.com>
2025-04-21 12:32:14 +0500

    creating multple instances from one class

--- Chapter 2 Practice/light_switch.py
+++ Chapter 2 Practice/light_switch.py
@@ -1,22 +1,30 @@
 class LighSwitch():
-    def __init__(self):
+    def __init__(self, name):
         self.switch_on = False
+        self.name = name
         
     def turn_on(self):
         self.switch_on = True
         
-    def tun_off(self):
+    def turn_off(self):
         self.switch_on = False
+        
+    def show(self):
+        print(f'{self.name} switch is {self.switch_on}')
 
 
 
-o_switch = LighSwitch()
+o_switch_1 = LighSwitch("Switch 1")
+o_switch_2 = LighSwitch("Switch 2")
 
-print(o_switch.switch_on)
+o_switch_1.show()
+o_switch_2.show()
 
-o_switch.turn_on()
+o_switch_1.turn_on()
+o_switch_2.turn_off()
 
-print(o_switch.switch_on)
+o_switch_2.show()
+o_switch_1.show()
 
 
 
